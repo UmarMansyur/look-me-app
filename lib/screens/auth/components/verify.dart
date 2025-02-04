b09207d6f46f918c6567a4afd15ebe952cc3002e
Umar Mansyur <umar.ovie@gmail.com>
2025-02-04 10:03:13 +0700

    push

--- lib/screens/auth/components/verify.dart
+++ lib/screens/auth/components/verify.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:look_me/screens/auth/components/remember_password.dart';
